e993f58d931f5c4fb30a1b7970b718eb174e7f66
Mingzhi Lin <mingzhi9@gmail.com>
2012-07-28 18:40:40 -0400

    specfunc gamma

--- specfunc/gamma.go
+++ specfunc/gamma.go
@@ -4,7 +4,20 @@ package specfunc
 //#include <gsl/gsl_sf_gamma.h>
 import "C"
 
+/* Log[Gamma(x)], x not a negative integer
+ * Uses real Lanczos method.
+ * Returns the real part of Log[Gamma[x]] when x < 0,
+ * i.e. Log[|Gamma[x]|].
+ */
 func Lngamma(x float64) (r float64) {
 	r = float64(C.gsl_sf_lngamma(C.double(x)))
+	return
+}
+
+/* Gamma(x), x not a negative integer
+ * Uses real Lanczos method.
+ */
+func Gamma(x float64) (r float64) {
+	r = float64(C.gsl_sf_gamma(C.double(x)))
 	return
 }
--- specfunc/gamma_test.go
+++ specfunc/gamma_test.go
@@ -65,3 +65,101 @@ func TestLngamma(t *testing.T) {
 		t.Errorf("Lngamma: result %.5f, expected %.5f\n", r, e)
 	}
 }
+
+func TestGamma(t *testing.T) {
+	var (
+		x float64
+		r float64
+		e float64
+		d float64
+	)
+
+	x = 1.0 + 1.0/4096.0
+	r = Gamma(x)
+	e = 0.9998591371459403421
+	d = 1e-6
+	if !assert.EqualFloat64(r, e, d, 1) {
+		t.Errorf("Gamma: result %.5f, expected %.5f\n", r, e)
+	}
+
+	x = 1.0 + 1.0/32.0
+	r = Gamma(x)
+	e = 0.9829010992836269148
+	d = 1e-6
+	if !assert.EqualFloat64(r, e, d, 1) {
+		t.Errorf("Gamma: result %.5f, expected %.5f\n", r, e)
+	}
+
+	x = 2.0 + 1.0/256.0
+	r = Gamma(x)
+	e = 1.0016577903733583299
+	d = 1e-6
+	if !assert.EqualFloat64(r, e, d, 1) {
+		t.Errorf("Gamma: result %.5f, expected %.5f\n", r, e)
+	}
+
+	x = 9.0
+	r = Gamma(x)
+	e = 40320.0
+	d = 1e-6
+	if !assert.EqualFloat64(r, e, d, 1) {
+		t.Errorf("Gamma: result %.5f, expected %.5f\n", r, e)
+	}
+
+	x = 10.0
+	r = Gamma(x)
+	e = 362880.0
+	d = 1e-6
+	if !assert.EqualFloat64(r, e, d, 1) {
+		t.Errorf("Gamma: result %.5f, expected %.5f\n", r, e)
+	}
+
+	x = 100.0
+	r = Gamma(x)
+	e = 9.332621544394415268e+155
+	d = 1e-6
+	if !assert.EqualFloat64(r, e, d, 1) {
+		t.Errorf("Gamma: result %.5f, expected %.5f\n", r, e)
+	}
+
+	x = 170.0
+	r = Gamma(x)
+	e = 4.269068009004705275e+304
+	d = 1e-6
+	if !assert.EqualFloat64(r, e, d, 1) {
+		t.Errorf("Gamma: result %.5f, expected %.5f\n", r, e)
+	}
+
+	x = 171.0
+	r = Gamma(x)
+	e = 7.257415615307998967e+306
+	d = 1e-6
+	if !assert.EqualFloat64(r, e, d, 1) {
+		t.Errorf("Gamma: result %.5f, expected %.5f\n", r, e)
+	}
+
+	x = -10.5
+	r = Gamma(x)
+	e = -2.640121820547716316e-07
+	d = 1e-6
+	if !assert.EqualFloat64(r, e, d, 1) {
+		t.Errorf("Gamma: result %.5f, expected %.5f\n", r, e)
+	}
+
+	x = -11.25
+	r = Gamma(x)
+	e = 6.027393816261931672e-08
+	d = 1e-6
+	if !assert.EqualFloat64(r, e, d, 1) {
+		t.Errorf("Gamma: result %.5f, expected %.5f\n", r, e)
+	}
+
+	x = -1.0 + 1.0/65536.0
+	r = Gamma(x)
+	e = -65536.42280587818970
+	d = 1e-6
+	if !assert.EqualFloat64(r, e, d, 1) {
+		t.Errorf("Gamma: result %.5f, expected %.5f\n", r, e)
+	}
+
+}
